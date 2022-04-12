4ab4b541c4c08aa1d0ee896331ea78ff7258f3d2
fishintokyo <kyokoumekawa@gmail.com>
2022-04-12 23:41:51 +0900

    add the weather app icon

--- source/index.js
+++ source/index.js
@@ -83,4 +83,10 @@ searchForm.addEventListener("submit", handleSubmit);
 let currentLocationButton = document.querySelector("#current-location-button");
 currentLocationButton.addEventListener("click", getCurrentLocation);
 
+let iconElement = document.querySelector("#icon");
+iconElement.setAttribute(
+  "src",
+  `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+);
+
 searchCity("Tokyo");
