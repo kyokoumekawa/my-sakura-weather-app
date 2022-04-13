75b21985f92fda40f4c03cffc609310eaeba88ef
fishintokyo <kyokoumekawa@gmail.com>
2022-04-13 22:30:17 +0900

    added the relevant icon to api

--- source/index.js
+++ source/index.js
@@ -31,6 +31,7 @@ function displayWeatherCondition(response) {
     "src",
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
 
   document.querySelector("#feels-like").innerHTML = Math.round(
     response.data.main.feels_like
